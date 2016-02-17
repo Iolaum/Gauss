3fa40c693fd2cbfae81de648c30a800bdc40af4f
Antonios Andronis <aa3e15@soton.ac.uk>
2016-02-17 22:04:24 +0000

    Adding comments in non_numerical

--- initialScripts/non_numerical.py
+++ initialScripts/non_numerical.py
@@ -13,15 +13,20 @@ import pandas as pd
 # Get the data from reading the training set
 data = pd.read_csv('../dataset/train.csv', sep=',', na_values='.') #read csv file, seperated by ;, na values exists
 
-
 # Find and print the names of all non-numerical features.
 print("Export the features with non-numerical data")
 
 non_numerical = []
+
+# data is of pandas's type Dataframe. It is a table that consists columns and rows.
 for column in data:
+	# column_series is of pandas type Series ( One-dimensional ndarray with axis labels)
 	column_series = data[column]
+
+	# dtype is property of a Series. It declares the data type of the values inside it.
 	if column_series.dtype not in ['int64', 'float64']:
 		first_item = column_series.iloc[0]
+		# Detect NaN values(they are calculated as Objects with type "float")
 		if type(first_item) is not float:
 			non_numerical.append(column)
 print("")
