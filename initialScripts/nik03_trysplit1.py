67ec9de05b207ba75075c88707584d6f624cb522
Nikolaos Perrakis <np4g15@soton.ac.uk>
2016-02-18 22:38:38 +0000

    napoleon created splitter

---
+++ initialScripts/nik03_trysplit1.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+# Python Script for Kaggle Competition
+# BNP Paribas Cardif claim management
+
+# Doesn't work!
+
+
+
+# Import Library & Modules
+import csv
+from sklearn.ensemble import RandomForestClassifier
+from sklearn import preprocessing
+import numpy as np  # linear algebraic manipulation
+import pandas as pd  # data processing, .csv files I/O
+
+output = []
+destfile = open('../dataset/train_splitted.csv', 'w')
+csv_writer = csv.writer(destfile)
+
+with open('../dataset/train.csv', 'r') as f:
+	reader = csv.reader(f)
+	c = 0
+	for row in reader:
+		csv_writer.writerow(row)
+		c += 1
+		if(c>1000):
+			break
+
+destfile.close()
