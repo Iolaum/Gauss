51767c926290aa3246f4dc810d598e595c32aa4e
Nikolaos Perrakis <np4g15@soton.ac.uk>
2016-04-24 17:09:43 +0100

    added numerical list

--- scripts/preprocessing/01_cat_val_handling.py
+++ scripts/preprocessing/01_cat_val_handling.py
@@ -11,14 +11,15 @@ import pandas as pd  # data processing, .csv files I/O
 # Read csv file, seperated by ',' 'nan' values exist
 # The result is of pandas's type Dataframe. It is a table that consists columns and rows.
 data = pd.read_csv('../../dataset/train.csv', sep=',', na_values='.')
-test = pd.read_csv('../../dataset/test.csv')
+#test = pd.read_csv('../../dataset/test.csv')
 
 del data['target']
 del data['ID']
-del test['ID']
+# del test['ID']
 
-# Populate a list with all the columns that have categorical values(non-numerical)
+# Populate 2 lists with all the columns that have categorical and numerical values.
 cat_cols = []
+num_cols = []
 
 for idx, column in data.iteritems():
     # column_series is of pandas type Series ( One-dimensional ndarray with axis labels)
@@ -28,8 +29,16 @@ for idx, column in data.iteritems():
     if column_series.dtype not in ['int64', 'float64']:
         # print(idx)
         # print(column.dtype)
+        # consistency check because of nan is not considered numerical
         # print(column_series.head(10))
 
         # if the column has categorical values, print and add to the list
         cat_cols.append(idx)
+    else:
+        num_cols.append(idx)
+        
+        
+print("Categorical Values are:")
 print(cat_cols)
+print("Numerical Values are:")
+print(num_cols)
