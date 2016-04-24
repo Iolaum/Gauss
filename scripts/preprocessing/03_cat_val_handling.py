6cfe7a5f1fd6b808eaaa1f51a46b991498fd3dc2
Antonios Andronis <aa3e15@soton.ac.uk>
2016-04-24 20:49:29 +0100

    Added comments on the script that handles the categorical data

--- scripts/preprocessing/03_cat_val_handling.py
+++ scripts/preprocessing/03_cat_val_handling.py
@@ -5,6 +5,8 @@ from __future__ import division
 # BNP Paribas Cardif claim management
 
 # This script handles the preprocessing of our data and more specifically the columns with categorical values.
+# Given the training dataset, we are converting the categorical values to the respective
+# conditional probability for output 1 given each of those values.
 
 # Import Library & Modules
 import pandas as pd  # data processing, .csv files I/O
@@ -18,21 +20,20 @@ data = pd.read_csv('../../dataset/train.csv', sep=',', na_values='.')
 # print(data)
 # print(test)
 
-
-# Populate 2 lists with all the columns that have categorical and numerical values.
-cat_cols = []
-num_cols = []
-
-total_no_of_samples = data.shape[0]
+# Create a dictionary that would have the format:
+# {
+#   v13: {
+#       A:0.15, B:0.80
+#   }
+# }
 
 total_cond_prob_matrix = dict()
 for idx, column in data.iteritems():
     # column_series is of pandas type Series ( One-dimensional ndarray with axis labels)
     column_series = data[idx]
 
-    # dtype is property of a Series. It declares the data type of the values inside it.
     # Get the columns with categorical values
-
+    # dtype is property of a Series. It declares the data type of the values inside it.
     if column_series.dtype not in ['int64', 'float64']:
         # print(idx)
         # print(column.dtype)
@@ -42,6 +43,8 @@ for idx, column in data.iteritems():
         cond_prob_matrix = dict()
         counter = 0
 
+        # For each category add 1 to output_1 given the category
+        # or 1 to output_0 given the category, depending on the output value
         for category in column_series:
             output_value = data['target'][counter]
             # print("Counter - " + str(counter))
@@ -60,12 +63,14 @@ for idx, column in data.iteritems():
                 cond_prob_matrix[category]['output_0'] += 1
             counter += 1
 
+        # Count total of distinct values for each categorical feature
         total_cond_prob_matrix[idx] = {'distinct_categories': len(cond_prob_matrix)}
 
         for categ, categ_class_sums in cond_prob_matrix.iteritems():
             # print(categ)
             # print(categ_class_sums)
 
+            # Calculate the conditional probability for output:1 for each value of each feature
             total_cond_prob_matrix[idx][categ] = categ_class_sums['output_1'] / (
                 categ_class_sums['output_1'] + categ_class_sums['output_0']
             )
